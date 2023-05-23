7c407ee83938ac42f8818917d9055b316e761502
Steve Alberty <alberty@neptunelabs.com>
2023-05-23 16:49:17 +0200

    change ssl and proxy settings

--- container/nginx/nginx-start.sh
+++ container/nginx/nginx-start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 if [ ! -d "/etc/nginx/certificates" ]
 then
@@ -9,11 +9,14 @@ if [ -w "/etc/nginx/certificates" ]
 then
   if [ ! -f "/etc/nginx/certificates/localhost.crt" ] || [ ! -f "/etc/nginx/certificates/localhost.key" ]
   then
-    openssl req -x509 -out /etc/nginx/certificates/localhost.crt \
+    echo "Create 'localhost' certificates"
+    openssl req -x509 \
+      -out /etc/nginx/certificates/localhost.crt \
       -keyout /etc/nginx/certificates/localhost.key \
       -newkey rsa:4096 -nodes -sha256 \
       -subj '/CN=localhost' -extensions EXT \
       -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+    echo "Certificates for 'localhost' generated for test purposes."
   fi
 fi
 
